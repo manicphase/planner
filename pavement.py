b7c4602b317c09e1b66a6e80525b28964f5d5232
jsalt <James Salt>
2015-01-26 22:06:29 +0000

    Add proper bootstrap venv support

--- pavement.py
+++ pavement.py
@@ -1,18 +1,25 @@
-from paver.easy import task, sh, needs
-from paver.virtual import virtualenv
+from paver.easy import task, sh, needs, Bunch, options
+from paver.virtual import bootstrap, virtualenv
 from paver.setuputils import setup
 
-setup(
-    name='Planner',
-    version='0.0.0',
-    packages=['planner'],
-    include_package_data=True,
-    install_requires=['SQLAlchemy', 'Flask', 'wtforms'],
-    author="James Salt",
-    author_email="saltpy+planner@gmail.com",
-    license="MIT",
-    tests_require=["virtualenv", "flake8"],
-    test_suite="test"
+options(
+    setup=dict(
+        name='Planner',
+        version='0.0.0',
+        packages=['planner'],
+        include_package_data=True,
+        install_requires=['SQLAlchemy', 'Flask', 'wtforms'],
+        author="James Salt",
+        author_email="saltpy+planner@gmail.com",
+        license="MIT",
+        tests_require=["virtualenv", "flake8"],
+        test_suite="test"
+    ),
+    virtualenv=Bunch(
+        packages_to_install=['SQLAlchemy', 'Flask', 'wtforms', 'virtualenv', 'flake8'],
+        script_name='bootstrap.py',
+        dest_dir='venv'
+    )
 )
 
 
@@ -32,4 +39,6 @@ def ci():
 
 @task
 def clean():
-    return sh('rm -rf ./*.egg*')
+    first = sh('rm -rf ./*.egg* venv build')
+    second = sh('find . -name "*.pyc" -delete')
+    return max([first, second])
