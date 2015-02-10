242ef0daf97386e64973ad29a60ceb2ecfd17141
Jordan Brennan <jordan.brennan@thetestpeople.com>
2015-02-10 15:39:06 +0000

    Added alembic with an update script to move from stable to current state

--- planner/config.py
+++ planner/config.py
@@ -1,8 +1,10 @@
 from os import environ
+from pkg_resources import resource_filename
 
+db_loc = resource_filename('planner', '../live.db')
 
 class StableConfig:
-    DBPATH = environ.get("PLANNER_DB", "sqlite:///live.db")
+    DBPATH = environ.get("PLANNER_DB", "sqlite:///{db_loc}".format(db_loc=db_loc))
     # TODO: Should these be strings or models/controllers/callables
     DISABLED_FEATURES = ['index', 'schedule', 'add_engagement',
                          'add_client', 'add_contact',
@@ -10,7 +12,7 @@ class StableConfig:
 
 
 class HeadConfig:
-    DBPATH = 'sqlite:///live.db'
+    DBPATH = "sqlite:///{db_loc}".format(db_loc=db_loc)
     DISABLED_FEATURES = []
 
 
