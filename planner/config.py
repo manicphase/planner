69f8867ba3864fb22ccaae1e1f295484dc512473
Jordan Brennan <jordan.brennan@thetestpeople.com>
2015-02-12 17:41:52 +0000

    Added data migration to alembiv revision. Run 'alembic upgrade head' in scripts folder to migrate

--- planner/config.py
+++ planner/config.py
@@ -3,8 +3,11 @@ from pkg_resources import resource_filename
 
 db_loc = resource_filename('planner', '../live.db')
 
+
 class StableConfig:
-    DBPATH = environ.get("PLANNER_DB", "sqlite:///{db_loc}".format(db_loc=db_loc))
+    DBPATH = environ.get(
+        "PLANNER_DB", "sqlite:///{db_loc}".format(db_loc=db_loc)
+    )
     # TODO: Should these be strings or models/controllers/callables
     DISABLED_FEATURES = ['index', 'schedule', 'add_engagement',
                          'add_client', 'add_contact',
